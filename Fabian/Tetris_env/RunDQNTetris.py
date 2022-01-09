00bf700f5fa5a048cc077d533f78c5a60b7a4e7d
Jason Aron Merit <76186383+JasonMerit@users.noreply.github.com>
2022-01-09 10:57:38 +0100

    aDDED PAUSE AND QUIT

--- Fabian/Tetris_env/RunDQNTetris.py
+++ Fabian/Tetris_env/RunDQNTetris.py
@@ -2,13 +2,40 @@ import numpy as np
 from TetEnv_States import Tetris
 from KerasModel import DQN
 import time
+import pygame
+clock = pygame.time.Clock()
 
 
 env = Tetris()
 agent = DQN(env=env)
 agent.train(games=0)
 actions, Features, score, done, _ = env.reset()
-while True:
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            dos_lag = 0
+            if event.key in [pygame.K_ESCAPE, pygame.K_q]:
+                run = False
+        
+        elif event.key == pygame.K_p:
+            pause = True
+            while pause:
+                clock.tick(40)
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+                    if event.type == pygame.KEYDOWN:
+                        if event.key in [pygame.K_ESCAPE, pygame.K_q]:
+                            pygame.quit()
+                            quit()
+                        elif event.key == pygame.K_p:
+                            pause = False
+    
+    
     action, feature = agent.take_action(actions, Features)
     actions, Features, score, done, _ = env.step(action)
     env.render()
