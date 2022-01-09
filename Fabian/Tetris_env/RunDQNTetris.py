91e82bee06c592bf593959cfa006e73b7a7a8102
Jason Aron Merit <76186383+JasonMerit@users.noreply.github.com>
2022-01-09 10:59:56 +0100

    Update RunDQNTetris.py

--- Fabian/Tetris_env/RunDQNTetris.py
+++ Fabian/Tetris_env/RunDQNTetris.py
@@ -18,22 +18,21 @@ while run:
         if event.type == pygame.KEYDOWN:
             dos_lag = 0
             if event.key in [pygame.K_ESCAPE, pygame.K_q]:
-                run = False
-        
-        elif event.key == pygame.K_p:
-            pause = True
-            while pause:
-                clock.tick(40)
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        quit()
-                    if event.type == pygame.KEYDOWN:
-                        if event.key in [pygame.K_ESCAPE, pygame.K_q]:
+                run = False        
+            elif event.key == pygame.K_p:
+                pause = True
+                while pause:
+                    clock.tick(40)
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
                             pygame.quit()
                             quit()
-                        elif event.key == pygame.K_p:
-                            pause = False
+                        if event.type == pygame.KEYDOWN:
+                            if event.key in [pygame.K_ESCAPE, pygame.K_q]:
+                                pygame.quit()
+                                quit()
+                            elif event.key == pygame.K_p:
+                                pause = False
     
     
     action, feature = agent.take_action(actions, Features)
