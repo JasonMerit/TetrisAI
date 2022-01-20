0fc27086b28dd0511cdf855318c0dd16d3b6f664
Fabian Scott <toellerandet@gmail.com>
2022-01-20 09:00:07 +0100

    K

--- Fabian/Tetris_env/State_based_clean/Results.py
+++ Fabian/Tetris_env/State_based_clean/Results.py
@@ -9,13 +9,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-dqn_trials = 'Trials_REAL'
-dqn_training = 'CSVDATA1'
+dqn_trials = 'Trialsbackup16'
+dqn_training = 'CSVDATAbackup'
 neat_trials = 'Trials_17_eve'
 neat_training = 'Training_17_eve'
 
-df = pd.read_csv(f'{dqn_trials}.csv')
-data = np.array(df)
+data = []
+for i in range(1, 7):
+    df = pd.read_csv(f'{dqn_trials}{i*6000}.csv')
+    tem = np.array(df)
+    data.append([np.mean(row) for row in tem[:, 3:]])
+print(len(data[0]))
 df2 = pd.read_csv(f'{dqn_training}')
 data2 = np.array(df2)
 df3 = pd.read_csv(f'{neat_trials}.csv')
@@ -25,39 +29,41 @@ data4 = np.array(df4)
 df5 = pd.read_csv('Trials_Linear.csv')
 data5 = np.array(df5)
 
-X = data[1:, 0]
-Y1 = np.array([np.mean(row) for row in data[1:, 3:]])
-Y2 = data2[:, 3]
-Y3 = np.array([np.mean(row) for row in data3[0:71, 1:]])
-Y4 = data4[0:71, 3]
-Y5 = np.array([np.mean(data5) for i in range(71)])
+X = data3[1:73, 0] * 50
+Y1 = np.array([n for m in data for n in m])
+Y2 = data2[1:, 3]
+Y3 = np.array([np.mean(row) for row in data3[0:72, 1:]])
+Y4 = data4[0:72, 3]
+Y5 = np.array([np.mean(data5) for i in range(72)])
 
 print(len(X), len(Y1), len(Y2), len(Y3), len(Y4))
-print(Y3[0], Y4[0])
-ci = 2/np.sqrt(len(data[0]) - 3)
+ci = float(2/np.sqrt(len(Y1) - 1))
 ci2 = 2/np.sqrt(len(data3[0]) - 1)
+ci3 = 2/np.sqrt(len(data5))
 fig, ax1 = plt.subplots()
 
-color1 = 'tab:blue'
+
+color1 = 'tab:red'
 color2 = 'tab:green'
-color3 = 'tab:red'
+color3 = 'tab:cyan'
 ax1.set_xlabel('Games played')
 ax1.set_ylabel('Average lines cleared')
 ax1.plot(X, Y1, color=color1)
 ax1.plot(X, Y3, color=color2)
 ax1.plot(X, Y5, color=color3)
-ax1.fill_between(X, (1-ci) * Y1, (1 + ci) * Y1, color='b', alpha=.1)
-ax1.fill_between(X, (1-ci2) * Y3, (1 + ci2) * Y3, color='g', alpha=.1)
+ax1.fill_between(X, (1-ci) * Y1, (1 + ci) * Y1, color=color1, alpha=.1)
+ax1.fill_between(X, (1-ci2) * Y3, (1 + ci2) * Y3, color=color2, alpha=.1)
+ax1.fill_between(X, (1-ci3) * Y5, (1 + ci3) * Y5, color=color3, alpha=.1)
 ax1.tick_params(axis='y')
-plt.ylim([0, 3000])
 
-ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+ax2 = ax1.twinx()
 
-ax2.set_ylabel('Training time')  # we already handled the x-label with ax1
+ax2.set_ylabel('Training time')
 ax2.plot(X, Y2, ':', color=color1)
 ax2.plot(X, Y4, ':', color=color2)
 ax2.tick_params(axis='y')
 
+ax2.legend()
 
-# fig.tight_layout()  # otherwise the right y-label is slightly clipped
+fig.tight_layout()
 plt.show()
--- Jason/PlayBest1.py
+++ Jason/PlayBest1.py
@@ -4,17 +4,22 @@ Created on Thu Jan  6 09:50:23 2022
 
 @author: Jason
 """
-from KerasModel import DQN
+import pickle
 from Tetris import Tetris
 import pygame
+import numpy as np
+
+agent = pickle.load(open('best.pickle_720', 'rb'))
+# W = np.array([-12.63, 6.6, -9.22, -19.77, -13.08, -10.49, -1.61, -24.04])
 
 rendering = True
 fps = 15
 
-env = Tetris(training=False, rendering=rendering, height=16)
+env = Tetris(False, None, [], rendering)
 
-agent = DQN(env, state_size=8, epsilon=0)
-agent.load('Backup_18500')
+clock = pygame.time.Clock()
+
+
 
 
 # Definitions and default settings
@@ -23,10 +28,10 @@ action_taken = False
 slow = True
 done = False
 
-
 while run:
     if rendering:
         clock.tick(fps)
+    
 
     # Process input
     for event in pygame.event.get():
@@ -53,18 +58,49 @@ while run:
                                 pause = False
                             elif event.key == pygame.K_p:
                                 pause = False
-
+    
     # Let agent determine and take next action
     # Find all final_states and evaluate them
     states = env.get_final_states()
     evaluations = env.get_evaluations(states)
-
+    
     # Pass the evaluation for each state into the NN
-    action, features = agent.take_action(states, evaluations)
-    # Go to best scored state
-    done = env.place_state(action)
+    outputs = [agent.activate(input) for input in evaluations]
+    # outputs = [np.dot(input, W) for input in evaluations]
+    
 
+    # Go to best scored state
+    best_index = outputs.index(max(outputs))
+    best_state = states[best_index]
+    done, _ = env.place_state(best_state) 
+    
+    while False and done:
+        clock.tick(40)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+                done = False
+            if event.type == pygame.KEYDOWN:
+                if event.key in [pygame.K_ESCAPE, pygame.K_q]:
+                    pygame.quit()
+                    run = False
+                    done = False
+                elif event.key == pygame.K_p:
+                    done = False
+    
+    
+    
+    # -12.63*lock_height+6.6*erodeded_cells-9.22*row_transitions-19.77*column_transitions\
+        # -13.08*holes-10.49*cum_wells-1.61*hole_depth-24.04*rows_holes
+    
+    
     if rendering:
         env.render()
-
+    
 pygame.quit()
+
+
+
+
+
