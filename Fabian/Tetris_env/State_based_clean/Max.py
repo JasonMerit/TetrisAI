9e78c8b208faa9ae05705fa516823fcc93ba891b
Fabian Scott <toellerandet@gmail.com>
2022-01-20 15:45:00 +0100

    k

---
+++ Fabian/Tetris_env/State_based_clean/Max.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('Best.csv')
+data = np.array(df)
+for i in range(2, 5):
+    df = pd.read_csv(f'Best{i}.csv')
+    data = np.append(data, np.array(df))
+
+df = pd.read_csv('Trials_Linear_16h.csv')
+data2 = np.array(df)
+
+print(len(data), len(data2))
+ci = 2/np.sqrt(len(data))
+mean = np.mean(data)
+ci2 = 2/np.sqrt(len(data2))
+mean2 = np.mean(data2)
+
+print('DQN:')
+print(f'Highest score: {np.max(data)}, Average score: {mean}, Confidence bounds: {ci * 100} %')
+print(f'Upper bound: {mean*ci + mean} \nLower bound: {-mean*ci+mean}')
+print('Linear model:')
+print(f'Highest score: {np.max(data2)}, Average score: {mean2}, Confidence bounds: {ci2 * 100} %')
+print(f'Upper bound: {mean2*ci2 + mean2} \nLower bound: {-mean2*ci2+mean2}')
+
--- Fabian/Tetris_env/State_based_clean/Results.py
+++ Fabian/Tetris_env/State_based_clean/Results.py
@@ -13,7 +13,7 @@ dqn_trials = 'Trialsbackup16'
 dqn_training = 'CSVDATAbackup'
 neat_trials = 'Trials_17_h16'
 neat_training = 'Training_17_eve'
-linear_lines = 'Trials_Linear16'
+linear_lines = 'Trials_Linear_16h.csv'
 
 data = []
 for i in range(1, 7):
@@ -37,7 +37,7 @@ Y4 = data4[:72, 3]
 Y5 = np.array([np.mean(data5) for i in range(72)])
 
 print(np.argmax(Y1) * 500, Y1)
-print(len(X), len(Y1), len(Y2), len(Y3), len(Y4))
+print(len(X), len(Y1), len(Y2), len(Y3), len(Y4), len(data5))
 ci = 2/np.sqrt(len(Y1) - 1)
 ci2 = 2/np.sqrt(len(data3[0]) - 1)
 ci3 = 2/np.sqrt(len(data5))
@@ -46,25 +46,24 @@ fig, ax1 = plt.subplots()
 color1 = 'tab:red'
 color2 = 'tab:green'
 color3 = 'tab:cyan'
-ax1.set_xlabel('Games played (1000s)')
+ax1.set_xlabel('Games played [1000s]')
 ax1.set_ylabel('Average lines cleared')
-ax1.plot(X, Y1, color=color1)
-ax1.plot(X, Y3, color=color2)
-ax1.plot(X, Y5, color=color3)
+lns1 = ax1.plot(X, Y1, color=color1, label='DQN lines cleared')
+lns2 = ax1.plot(X, Y3, color=color2, label='NEAT lines cleared')
+lns3 = ax1.plot(X, Y5, color=color3, label='Linear model lines cleared')
 ax1.fill_between(X, (1-ci) * Y1, (1 + ci) * Y1, color=color1, alpha=.1)
-ax1.fill_between(X, (1-ci2) * Y3, (1 + ci2) * Y3, color=color2, alpha=.1)
+ax1.fill_between(X, (1-ci2) * Y3, (1 + ci2) * Y3, color=color2, alpha=.2)
 ax1.fill_between(X, (1-ci3) * Y5, (1 + ci3) * Y5, color=color3, alpha=.1)
 ax1.tick_params(axis='y')
-# ax1.legend(['DQN lines cleared', 'NEAT lines cleared'], 'upper_left')
 
 ax2 = ax1.twinx()
 
-ax2.set_ylabel('Training time')
-ax2.plot(X, Y2, ':', color=color1)
-ax2.plot(X, Y4, ':', color=color2)
+ax2.set_ylabel('Training time [hours]')
+lns4 = ax2.plot(X, Y2/60, ':', color=color1, label='DQN training time')
+lns5 = ax2.plot(X, Y4/60, ':', color=color2, label='NEAT training time')
 ax2.tick_params(axis='y')
-
-ax2.legend(['DQN training time', 'NEAT training time'])
-
+lns = lns1+lns2+lns3+lns4+lns5
+labs = [l.get_label() for l in lns]
+ax1.legend(lns, labs, loc=0)
 fig.tight_layout()
 plt.show()
